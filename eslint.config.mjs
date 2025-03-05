9eaa94632054c8ba3f80f8bf5b9c09e50686f8ce
jjanglove <kek2973@bizbee.co.kr>
2025-03-05 12:21:07 +0900

    init: tailwindcss eslint 적용 작업

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import stylisticJs from "@stylistic/eslint-plugin";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
+import eslintPluginTailwindCSS from "eslint-plugin-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -34,6 +35,12 @@ export default tseslint.config(
     semi: true,
     commaDangle: "always-multiline",
   }),
+  {
+    plugins: {
+      "tailwindcss": eslintPluginTailwindCSS,
+    },
+    rules: eslintPluginTailwindCSS.configs.recommended.rules,
+  },
   {
     plugins: {
       "react-hooks": reactHooks,
