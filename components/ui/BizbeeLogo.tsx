229625725635f9895d093d5eb97f62b535168c3b
jjanglove <kek2973@bizbee.co.kr>
2025-03-24 10:16:42 +0900

    fix: 로고 타이틀 변경

--- components/ui/BizbeeLogo.tsx
+++ components/ui/BizbeeLogo.tsx
@@ -7,8 +7,8 @@ function BizbeeLogo() {
       <Logo width="44px" height="44px" />
       <h1 className="flex flex-col">
         <strong className="md:text-xl max-md:text-md font-bold">Bizbee</strong>
-        <span className="md:text-sm max-md:text-xs font-semibold">
-          메시지 대시보드
+        <span className="md:text-sm max-md:text-xs font-semibold text-nowrap">
+          Push Dashboard
         </span>
       </h1>
     </div>
