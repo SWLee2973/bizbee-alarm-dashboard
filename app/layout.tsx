07ea505ddb3020d340fd7f7c37bd18dc0f0fb189
jjanglove <kek2973@bizbee.co.kr>
2025-04-09 15:04:54 +0900

    feat: health-check 페이지(non-auth)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "@/globals.css";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script";
 
 const pretendard = localFont({
   src: "../public/fonts/PretendardVariable.woff2",
@@ -25,6 +26,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className={pretendard.variable}>
+      {/* <head>
+        <Script id="theme-script" strategy="beforeInteractive">
+          {`
+            (function() {
+              try {
+                // localStorage에서 저장된 테마 값을 불러오고, 없다면 기본값 'light'를 사용
+                var theme = localStorage.getItem('theme') || 'light';
+                document.documentElement.setAttribute('data-theme', theme);
+              } catch (e) {
+                console.error('테마 설정 오류:', e);
+              }
+            })();
+          `}
+        </Script>
+      </head> */}
       <body className={`${pretendard.className} antialiased`}>
         <QueryProvider>
           <SessionProvider>
